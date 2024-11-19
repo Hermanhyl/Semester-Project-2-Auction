9e2e6bfe5b970f5838de0d29f43895c64c4dd9a8
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-11-19 17:14:32 +0100

    Instalation of vite, tailwind, postcss and autoprefixer

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import {resolve} from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  appType: "mpa",
+  base: "",
+  build: {
+    target: "esnext", 
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./index.html"),
+        login: resolve(__dirname, "./auth/login/index.html"),
+      },
+    },
+  },
+});
