bff5ccdec6c2cc252ee73ad1e7b905007b89db1f
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-11-20 10:10:55 +0100

    added profile to the rollupOptions

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "./index.html"),
         login: resolve(__dirname, "./auth/login/index.html"),
+        profile: resolve(__dirname, "./profile/index.html"),
       },
     },
   },
