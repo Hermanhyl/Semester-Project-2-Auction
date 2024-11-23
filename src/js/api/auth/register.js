0c74bdbb18a0da981f416fd265565128316de058
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-11-23 16:49:37 +0100

    Added register function with fetch

---
+++ src/js/api/auth/register.js
@@ -0,0 +1,27 @@
+import { API_AUTH_REGISTER } from "../constants";
+import { headers } from "../headers";
+
+export async function register({
+    name,
+    email,
+    password,
+}) {
+    const body = {
+        name: name,
+        email: email,
+        password: password,
+    }
+    try {
+        const response = await fetch(API_AUTH_REGISTER, {
+            method: "POST",
+            headers: headers(),
+            body: JSON.stringify(body),
+        });
+        if (response.ok) {
+            alert(`succsessfully registered "${name}"`);
+            window.location.href = "/auth/login/"
+        }
+    } catch(Error) {
+        alert("Something went wrong");
+    }
+}
