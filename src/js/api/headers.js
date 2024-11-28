8417e4acec72c40c54f29cdb0ca944d179f7664f
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-11-28 12:15:56 +0100

    fixed parse issue for local storage

---
+++ src/js/api/headers.js
@@ -0,0 +1,18 @@
+import { API_KEY } from "./constants";
+
+const accessToken = JSON.parse(localStorage.getItem('token'));
+
+export function headers() {
+    const headers = new Headers();
+
+    if (API_KEY) {
+        headers.append("X-Noroff-API-Key", API_KEY);
+    }
+
+    if (accessToken) {
+        headers.append("Authorization", `Bearer ${accessToken}`)
+    }
+
+    headers.append("Content-Type", "application/json");
+    return headers;
+}
