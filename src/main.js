37c35f95f08226b7c7ce6d4a79a29fac984644e4
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-11-28 12:16:12 +0100

    added navbar to main

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import "./css/style.css";
-
 import router from "./js/router";
+import { generateNavbar } from "./js/utilitis/navBar";
 
 await router(window.location.pathname);
 
-
+generateNavbar()
