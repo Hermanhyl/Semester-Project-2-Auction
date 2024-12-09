f3ead1ec494c4dfbf173bb45ed2e8caa4b8c9fd2
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-12-09 14:51:27 +0100

    added auth, register, editProfile, post, createPost and editpost to the rollupOptions

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,14 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "./index.html"),
+        auth: resolve(__dirname, "./auth/index.html"),
+        register: resolve(__dirname, "./auth/register/index.html"),
         login: resolve(__dirname, "./auth/login/index.html"),
         profile: resolve(__dirname, "./profile/index.html"),
+        editProfile: resolve(__dirname, "./profile/edit/index.html"),
+        post: resolve(__dirname, "./post/index.html"),
+        createPost: resolve(__dirname, "./post/create/index.html"),
+        editPost: resolve(__dirname, "./post/edit/index.html"),
       },
     },
   },
