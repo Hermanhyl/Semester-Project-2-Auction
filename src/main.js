863410504ea087b12bc382dd0b473adf6dfc2c19
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-12-09 15:06:06 +0100

    added setLogoutListener

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import "./css/style.css";
 import router from "./js/router";
+import { setLogoutListener } from "./js/ui/auth/logout";
 import { generateNavbar } from "./js/utilitis/navBar";
+
 
 await router(window.location.pathname);
 
 generateNavbar()
+
+setLogoutListener(); 
