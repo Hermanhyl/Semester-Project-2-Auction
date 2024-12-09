8e2ab4520ba071e0f78edebf096f4d8cbca5c4d0
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-12-09 11:14:34 +0100

    onBid and showBid function. Targeting form and modal

---
+++ src/js/ui/listing/bid.js
@@ -0,0 +1,40 @@
+import { fetchBid, fetchBidders } from "../../api/listing/bid";
+
+
+export const onBid = async (event, id) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target); 
+    const amount = Number(formData.get("bidInput"));
+
+    if (!amount || amount <= 0) {
+        console.error("Invalid bid amount");
+        return; 
+    }
+
+    const bidResponse = await fetchBid(amount, id); 
+        
+};
+
+export const showBidders = async (id) => {
+    const modal = document.getElementById("biddersModal");
+    const biddersList = document.getElementById("biddersList");
+
+    biddersList.innerHTML = "";
+
+    
+        const bidders = await fetchBidders(id);
+        console.log("Bidders fetched:", bidders);
+
+        if (bidders.length === 0) {
+            biddersList.innerHTML = "<p>No bids yet.</p>";
+        } else {
+            bidders.forEach((bid) => {
+                const bidderItem = document.createElement("p");
+                bidderItem.innerText = `User: ${bid.bidder.name}, Amount: ${bid.amount}`;
+                biddersList.appendChild(bidderItem);
+            });
+        }
+    
+    modal.classList.remove("hidden");
+};
