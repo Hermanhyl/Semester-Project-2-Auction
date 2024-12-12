58cf2f846398226e77c883f878cff990c9e03489
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-12-12 16:30:45 +0100

    deleted concole.log

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -9,7 +9,6 @@ export async function onlogin(event) {
         email: formData.get("email"),
         password: formData.get("password"),
     }
-    console.log("login data",loginData);
     
     login(loginData);
 }
