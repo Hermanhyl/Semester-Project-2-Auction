2ca95f34024a10e6852b4303ef40aa1684ae610a
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-12-12 16:16:00 +0100

    added footer

---
+++ src/js/utilitis/footer.js
@@ -0,0 +1,17 @@
+export function createFooter() {
+    const footer = document.createElement('footer');
+    footer.style.backgroundColor = '#2B2D42'; 
+    footer.style.color = '#FFFFFF'; 
+    footer.style.padding = '1rem';
+    footer.style.textAlign = 'center';
+    footer.style.fontSize = '14px';
+    footer.style.position = 'absolut';
+    footer.style.bottom = '0';
+    footer.style.width = '100%';
+
+    footer.textContent = '© 2024 All rights reserved.';
+
+    document.body.appendChild(footer);
+}
+
+createFooter() 
