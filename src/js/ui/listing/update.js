8aca1e4f3537ef5a044c2119ee9160465b082158
Hermanhyl <60390638+Hermanhyl@users.noreply.github.com>
2024-12-13 18:16:09 +0100

    changed call and import

--- src/js/ui/listing/update.js
+++ src/js/ui/listing/update.js
@@ -1,4 +1,4 @@
-import { updateListing } from "../../api/listing/update.js";
+import { updateListing } from "../../api/listing/update";
 
 export async function onUpdateListing(event, postId) {
     event.preventDefault();
